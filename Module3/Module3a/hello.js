2ffce26088b1916cb25a2ffd31f2bf9f041f1c6f
Olamide Oluwole <132096317+LammieQA@users.noreply.github.com>
2023-10-18 05:12:05 +0100

    fixing alert on server

--- Module3/Module3a/hello.js
+++ Module3/Module3a/hello.js
@@ -1,4 +1,7 @@
-
+//Fun JavaScript Facts to Share at Parties
+//JavaScript took just 10 days to develop.
+//JavaScript was probably named after Java.
+//In reality, there is no longer one “true” JavaScript.
 
         console.log("Hello, world!")
     
