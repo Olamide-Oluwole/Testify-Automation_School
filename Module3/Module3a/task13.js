2184a6215d83ca0a9d5dc7e16da0836d75a5bf29
Olamide Oluwole <132096317+LammieQA@users.noreply.github.com>
2023-10-30 23:47:08 +0100

    Object literals

---
+++ Module3/Module3a/task13.js
@@ -0,0 +1,10 @@
+// A program that shows a JS book object using object literals
+
+const books =
+{
+    title : "The Quality Advocate",
+    description: "This book speaks about quality as a culture",
+    numberOfPages: 100,
+    author: "Olamide Oluwole",
+    reading: true
+}
