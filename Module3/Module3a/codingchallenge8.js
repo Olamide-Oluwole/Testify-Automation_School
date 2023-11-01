82f51762ee51f573f45adf8dda8209c6b91d5b03
Olamide Oluwole <132096317+LammieQA@users.noreply.github.com>
2023-11-01 23:10:01 +0100

    returns boolean if divisible by 10

---
+++ Module3/Module3a/codingchallenge8.js
@@ -0,0 +1,9 @@
+//Return a Boolean if a number is divisible by 10.
+const inputNumber = 120;
+
+function returnBoolean(number) {
+    return (number % 10 == 0) 
+    }
+
+
+console.log(returnBoolean(inputNumber));
