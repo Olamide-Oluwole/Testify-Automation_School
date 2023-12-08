2ffb11b9883901ac328681629caabfff17f42b16
Olamide Oluwole <132096317+LammieQA@users.noreply.github.com>
2023-12-08 08:22:13 +0100

    Task 10

---
+++ Module4/Task/src/Task10.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Task10 {
+    public static void main(String[] args) {
+        try (Scanner slackChannel = new Scanner(System.in)) {
+            Task10 taskInstance = new Task10();
+
+            while (true) {
+                String text = taskInstance.getUserInput(slackChannel);
+                taskInstance.verifyUsersSlackChannel(text);
+                if (text.equalsIgnoreCase("Testify")) {
+                    break;
+                }
+            }
+        }
+    }
+
+    private String getUserInput(Scanner scanner) {
+        System.out.println("What training did you sign up for?");
+        return scanner.nextLine();
+    }
+
+    public void verifyUsersSlackChannel(String userInput) {
+        if (userInput.equalsIgnoreCase("Testify")) {
+            System.out.println("You are welcome to the Testify family");
+        } else {
+            System.out.println("Sorry! Channel not allowed");
+        }
+    }
+}
