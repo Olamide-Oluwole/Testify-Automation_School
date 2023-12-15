3a1c536d9e94db01afcd7af8bec5b05395679d60
Olamide Oluwole <132096317+LammieQA@users.noreply.github.com>
2023-12-15 11:11:33 +0100

    Task 17A

---
+++ Module4/Task/src/Task17A.java
@@ -0,0 +1,18 @@
+public abstract class Task17A {
+    //  blueprint for your company's login page
+// Common fields for all login pages
+    String username;
+    String password;
+    String forgotpassword;
+// Common method for all login pages
+    public abstract void signInBtn(String username, String password);
+
+    public void continueToHome()
+    {
+    }
+
+    public void oauthBtn(){
+    }
+
+}
+
