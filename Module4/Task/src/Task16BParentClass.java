e9112d24c7b1b461be6a0f78f5605a204e5fe9ff
Olamide Oluwole <132096317+LammieQA@users.noreply.github.com>
2023-12-15 08:20:32 +0100

    Task 16B

---
+++ Module4/Task/src/Task16BParentClass.java
@@ -0,0 +1,12 @@
+public class Task16BParentClass {
+
+    // Method with one argument
+    void getName(String fullname) {
+        System.out.println("My full name is : " + fullname);
+    }
+
+    // Method with two arguments
+    void getFullname(String firstname, String lastname) {
+        System.out.println("My first and last names are: " + firstname + ", " + lastname);
+    }
+}
