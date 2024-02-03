ff8198ce6af6f654c78d126266383cdee9f6aba1
Olamide Oluwole <132096317+LammieQA@users.noreply.github.com>
2024-02-03 01:38:26 +0100

    task 4

---
+++ Module4/Module4c/Module4c/task4.py
@@ -0,0 +1,15 @@
+# Declare two numbers with any values
+number1 = 10
+number2 = 5
+
+# Add the two numbers
+sum_number = number1 + number2
+print(f"Addition Result: {sum_number}")
+
+# Subtract the second number from the first
+subtract_number = number1 - number2
+print(f"Subtraction Result: {subtract_number}")
+
+# Multiply the two numbers
+multiply_result = number1 * number2
+print(f"Multiplication Result: {multiply_result}")
