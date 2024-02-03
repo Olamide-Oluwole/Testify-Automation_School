41a7c81f6eb3aa385fbfa86bb6692574ae31eefa
Olamide Oluwole <132096317+LammieQA@users.noreply.github.com>
2024-02-03 01:52:34 +0100

    task9

---
+++ Module4/Module4c/Module4c/task9.py
@@ -0,0 +1,11 @@
+# Function that prints out "Hello World"
+def print_hello_world():
+    print("Hello World")
+
+# Function that does nothing (empty function)
+def do_nothing():
+    pass
+
+# Invoke/call both functions
+print_hello_world()
+do_nothing()
