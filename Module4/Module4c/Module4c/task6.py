c21efa6df0bd363cf1bbe250d8ef25dcaa8418df
Olamide Oluwole <132096317+LammieQA@users.noreply.github.com>
2024-02-03 01:43:53 +0100

    task 6

---
+++ Module4/Module4c/Module4c/task6.py
@@ -0,0 +1,16 @@
+# Declare two numbers with any values
+number1 = 60
+number2 = 80
+
+# Check if the first number is greater than 50 and the second number is less than 100
+if number1 > 50 and number2 < 100:
+    print("Evaluation is True")
+else:
+    print("Evaluation is False")
+
+# Declare a string with value "python"
+my_string = "python"
+
+# Check if the string is equal to "testify" or "python"
+if my_string == "testify" or my_string == "python":
+    print("Yay!! The string is a Testify Python")
