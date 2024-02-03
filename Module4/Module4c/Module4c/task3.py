bfe58dec732d03acfa94109c790be1115e557ec6
Olamide Oluwole <132096317+LammieQA@users.noreply.github.com>
2024-02-03 01:32:56 +0100

    task 3

---
+++ Module4/Module4c/Module4c/task3.py
@@ -0,0 +1,12 @@
+# Declare a raw string
+raw_string = r"This is a raw string \n"
+
+# Declare two strings with any value
+string1 = "Good"
+string2 = "Morning"
+
+# Concatenate the two strings
+concatenated_string = string1 + " " + string2
+
+# Print out the value of the concatenated strings
+print(concatenated_string)
