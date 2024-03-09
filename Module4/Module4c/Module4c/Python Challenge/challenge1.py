105083f94dbfaab07fee75e90371ebbdbe89893a
Olamide Oluwole <132096317+LammieQA@users.noreply.github.com>
2024-03-09 18:39:38 +0100

    challenge 1

---
+++ Module4/Module4c/Module4c/Python Challenge/challenge1.py
@@ -0,0 +1,12 @@
+def calculate_sum(arr):
+    return sum(arr)
+
+
+# declare the array
+my_array = [1, 6, 8, 5, 11]
+
+# call the function to calculate the sum
+total_sum = calculate_sum(my_array)
+
+# print out the sum of numbers
+print("Sum of the numbers in the array:", total_sum)
